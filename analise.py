0013ee3568773375f108df4af8f5963ae1d23162
dorlegbs <gabidorle@gmail.com>
2026-05-05 14:47:10 -0300

    Update analise.py

--- analise.py
+++ analise.py
@@ -5,12 +5,5 @@ df = pd.read_csv("deputados_2022.csv")
 
 st.dataframe(df)
 
-
-df_masculino = df[df["sexo"] == "M"]
-nomes_masculinos = df_masculino["nome"]
-
-print(nomes_masculinos)
-
-nomes_masculinos.to_csv("nomes_masculinos.csv", index=False)
-
+partido_input = input("Digite o partido ").upper()
 
