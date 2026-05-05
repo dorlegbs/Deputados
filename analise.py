1b1c92705c146c051656feeaef73ca8385bf817f
dorlegbs <gabidorle@gmail.com>
2026-05-05 14:40:37 -0300

    Update analise.py

--- analise.py
+++ analise.py
@@ -5,8 +5,8 @@ url = "https://github.com/dorlegbs/Deputados/blob/main/deputados_2022.csv"
 df = pd.read_csv(url)
 
 df_masculino = df[df["sexo"] == 'M']
-nome_masculino = df_masculino["nome"]
+nomes_masculinos = df_masculino["nome"]
 
-print(nome_masculino)
+print(nomes_masculinos)
 
 nomes_masculinos.to_csv("nomes_masculinos.csv", index=False)
