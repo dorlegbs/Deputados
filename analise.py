eaaf909ff2aecb38f286235ab91f23ba171ef7d7
dorlegbs <gabidorle@gmail.com>
2026-05-05 14:41:22 -0300

    Update analise.py

--- analise.py
+++ analise.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-url = "https://github.com/dorlegbs/Deputados/blob/main/deputados_2022.csv"
+url = "https://raw.github.com/dorlegbs/Deputados/blob/main/deputados_2022.csv"
 
 df = pd.read_csv(url)
 
