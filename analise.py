05268591b7a18bc0a64bbc5b14c8aa0dac821d12
dorlegbs <gabidorle@gmail.com>
2026-05-05 14:45:09 -0300

    Update analise.py

--- analise.py
+++ analise.py
@@ -6,3 +6,11 @@ df = pd.read_csv("deputados_2022.csv")
 st.dataframe(df)
 
 
+df_masculino = df[df["sexo"] == "M"]
+nomes_masculinos = df_masculino["nome"]
+
+print(nomes_masculinos)
+
+nomes_masculinos.to_csv("nomes_masculinos.csv", index=False)
+
+
