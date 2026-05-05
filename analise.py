8e494aba7c73b56cfec8c8d9e9eef7109b85fbc8
dorlegbs <gabidorle@gmail.com>
2026-05-05 14:52:14 -0300

    Update analise.py

--- analise.py
+++ analise.py
@@ -7,5 +7,17 @@ st.dataframe(df)
 
 st.title("Consulta de Deputados por Partido")
 
-partido_input = st.text_input("Digite o partido ").upper()
+partido_input = st.text_input("Digite a sigla do partido ").upper()
 
+if partido_input:
+    partido_input = partido_input.upper()
+
+    # filtrar
+    df_filtrado = df[df["partido"].str.upper() == partido_input]
+
+    if df_filtrado.empty:
+        st.warning("Nenhum deputado encontrado para esse partido.")
+    else:
+        st.subheader(f"Deputados do partido {partido_input}")
+        st.write(df_filtrado["nome"])
+        st.write(f"Total: {df_filtrado.shape[0]}")
