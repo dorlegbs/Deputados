157dcd5174ec86a473d3d839f0d7caa9384b69f7
dorlegbs <gabidorle@gmail.com>
2026-05-05 14:44:41 -0300

    Update analise.py

--- analise.py
+++ analise.py
@@ -1,11 +1,8 @@
 import streamlit as st
-
 import pandas as pd
-
-url = "https://raw.github.com/dorlegbs/Deputados/blob/main/deputados_2022.csv"
 
 df = pd.read_csv("deputados_2022.csv")
 
-df.datafram(df)
+st.dataframe(df)
 
 
